50d2397559ea8222029ba982ba842759ad1d23e3
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-11-12 00:54:09 +0530

    Backend Folder Structure

---
+++ Controllers/postControllers.js
@@ -0,0 +1,7 @@
+exports.getAllPosts = () => {
+
+}
+
+exports.createPost = () => {
+    
+}
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express();
 
-const PORT = 5000;
+require('dotenv').config()
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log("Server running on PORT :- " + PORT);
