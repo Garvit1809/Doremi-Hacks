7df47d9fe708579ca9ff72abf410862c314d4e01
Adarsh Dubey <dubeyadarshmain@gmail.com>
2022-11-13 16:44:19 +0530

    added fake data

--- client/src/components/MusicLabel.js
+++ client/src/components/MusicLabel.js
@@ -5,28 +5,28 @@ export const MusicLabelsData = [
         listening: '125,000'
     },
     {
-        name: 'Sony',
-        artists: '41',
-        listening: '125,000'
+        name: 'T-Series',
+        artists: '97',
+        listening: '361,250'
     },
     {
-        name: 'Sony',
-        artists: '41',
-        listening: '125,000'
+        name: 'Mass Appeal',
+        artists: '23',
+        listening: '57,000'
     },
     {
-        name: 'Sony',
-        artists: '41',
-        listening: '125,000'
+        name: 'Universal Music',
+        artists: '49',
+        listening: '198,870'
     },
     {
-        name: 'Sony',
-        artists: '41',
-        listening: '125,000'
+        name: 'Saregama Studio',
+        artists: '74',
+        listening: '363,000'
     },
     {
-        name: 'Sony',
-        artists: '41',
-        listening: '125,000'
+        name: 'Royal Opera House',
+        artists: '12',
+        listening: '36,250'
     },
 ]
--- client/src/components/TrendingData.js
+++ client/src/components/TrendingData.js
@@ -7,38 +7,38 @@ export const trendingData = [
         duration: '5:41'
     },
     {
-        name: 'Deathbed',
-        performerName: 'Powfu and Beabadoobee',
-        popularity: '241',
-        publishedOn: '02/11',
-        duration: '5:41'
+        name: 'Arcade',
+        performerName: 'Duncan Laurence',
+        popularity: '454',
+        publishedOn: '07/03',
+        duration: '3:03'
     },
     {
-        name: 'Deathbed',
-        performerName: 'Powfu and Beabadoobee',
-        popularity: '241',
-        publishedOn: '02/11',
-        duration: '5:41'
+        name: 'Choo Lo',
+        performerName: 'The Local Train',
+        popularity: '36',
+        publishedOn: '05/21',
+        duration: '3:53'
     },
     {
-        name: 'Deathbed',
-        performerName: 'Powfu and Beabadoobee',
-        popularity: '241',
-        publishedOn: '02/11',
-        duration: '5:41'
+        name: 'Main Rang Sharbaton Ka',
+        performerName: 'Garvit Varshney',
+        popularity: '525',
+        publishedOn: '09/11',
+        duration: '4:31'
     },
     {
-        name: 'Deathbed',
-        performerName: 'Powfu and Beabadoobee',
-        popularity: '241',
-        publishedOn: '02/11',
-        duration: '5:41'
+        name: 'Samjhawan',
+        performerName: 'Garvit Varshney',
+        popularity: '785',
+        publishedOn: '07/01',
+        duration: '4:26'
     },
     {
-        name: 'Deathbed',
-        performerName: 'Powfu and Beabadoobee',
-        popularity: '241',
-        publishedOn: '02/11',
-        duration: '5:41'
+        name: 'Jag Ghoomeya',
+        performerName: 'Garvit Varshney',
+        popularity: '456',
+        publishedOn: '28/12',
+        duration: '4:37'
     }
 ]
